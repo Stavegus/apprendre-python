c1acbd8984446775b8181a90df95087011c1b0d0
alexis <alexis.flanquart@gmail.com>
2021-08-27 00:23:35 +0200

    Les bases de python

---
+++ main.py
@@ -0,0 +1,32 @@
+# Commentaire d'une ligne
+
+"""
+Commentaire de
+plusieurs ligne
+
+"""
+
+# Variable
+# chaîne de caractere
+prenom = "Jose"
+# Nombre
+age_utilisateur = 23
+# Decimal
+pi = 3.14
+# Boolean = Vrai ou Faux
+boolean = True
+
+# Permet d'afficher un message dans la console
+print("On affiche un message")
+
+# Input récupérer les informations saisie par l'utilisateur
+# On demande l'age et le nom de l'utilisateur
+nom = input("Quel est votre nom ? ")
+age = input("Quel est votre age ? ")
+
+# On affiche dens la console
+print(f"Bonjour {nom} vous avez {str(age)} ans")
+
+# Convertir un entier en chaîne de caractere
+
+str(age_utilisateur)
